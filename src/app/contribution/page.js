66a911cca3a71e41da90d32454186a8d3bdc1d22
komoria2025-code <komor.ia2025@gmail.com>
2026-06-13 12:34:29 +0100

    Update page.js

--- src/app/contribution/page.js
+++ src/app/contribution/page.js
@@ -357,7 +357,7 @@ export default function ContributionPage() {
               {
                 name: 'Shimaore',
                 region: 'Mayotte',
-                flag: '🇫🇷',
+                flag: '🇰🇲',
                 default: false,
               },
             ].map((d, i) => (
